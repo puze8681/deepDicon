8dd163bbd7c931f362cc16bd01b564efd3d24b7c
puze8681 <rsef0601@naver.com>
2016-11-26 20:23:46 +0900

    Update SplashActivity.java

--- app/src/main/java/com/example/parktaejun/nowhere/SplashActivity.java
+++ app/src/main/java/com/example/parktaejun/nowhere/SplashActivity.java
@@ -33,7 +33,7 @@ public class SplashActivity extends Activity {
             public void run() {
                 finish();       // 3 초후 이미지를 닫아버림
             }
-        }, 3000);햐
+        }, 3000);
 
     }
 }
